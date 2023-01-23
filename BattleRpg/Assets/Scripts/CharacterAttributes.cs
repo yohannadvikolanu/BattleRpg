2587b9aae6d4fc8b025bf41ae0b3305a5bfdaa4b
Yohann.Advikolanu <yohann.advikolanu@gmail.com>
2023-01-23 05:50:23 +0000

    Implementation for character attributes

---
+++ BattleRpg/Assets/Scripts/CharacterAttributes.cs
@@ -0,0 +1,28 @@
+namespace BattleRpg.Character
+{
+    /// <inheritdoc/>
+    internal sealed class CharacterAttributes : ICharacterAttributes
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CharacterAttributes"/> class.
+        /// </summary>
+        /// <param name="health">The value for the total health of the character.</param>
+        /// <param name="attackPower">The value for the total attack power of the character.</param>
+        /// <param name="level">The value for the level of the character.</param>
+        public CharacterAttributes(float health, float attackPower, int level)
+        {
+            Health = health;
+            AttackPower = attackPower;
+            Level = level;
+        }
+
+        /// <inheritdoc/>
+        public float Health { get; }
+
+        /// <inheritdoc/>
+        public float AttackPower { get; }
+
+        /// <inheritdoc/>
+        public int Level { get; }
+    }
+}
